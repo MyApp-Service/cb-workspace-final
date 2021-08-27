454e9db930b46072b38569bcfc1847f3da7854a1
MyApp-Service <manikandan1881991@gmail.com>
2021-08-27 15:44:22 +0530

    main test class commented

--- circuit-breaker/src/test/java/com/tsb/cb/AppTests.java
+++ circuit-breaker/src/test/java/com/tsb/cb/AppTests.java
@@ -1,29 +1,35 @@
-package com.tsb.cb;
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.tsb.cb.config.CircuitBreakerAutoConfiguration;
-import com.tsb.cb.config.CircuitBreakerConfigProperties;
-import com.tsb.cb.config.CircuitBreakerProperties;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {CircuitBreakerApplication.class, CircuitBreakerAutoConfiguration.class })
-@WebAppConfiguration
-//@EnableAutoConfiguration()
-@EnableConfigurationProperties(value = { CircuitBreakerProperties.class})
-@TestPropertySource(locations="classpath:application-test.yml")
-
-public class AppTests {
-
-	/*
-	 * public static void main(String[] args) {
-	 * 
-	 * }
-	 */
-}
+/*
+ * package com.tsb.cb;
+ * 
+ * import org.junit.runner.RunWith; import
+ * org.springframework.boot.autoconfigure.EnableAutoConfiguration; import
+ * org.springframework.boot.context.properties.EnableConfigurationProperties;
+ * import org.springframework.test.context.ContextConfiguration; import
+ * org.springframework.test.context.TestPropertySource; import
+ * org.springframework.test.context.junit4.SpringJUnit4ClassRunner; import
+ * org.springframework.test.context.web.WebAppConfiguration;
+ * 
+ * import com.tsb.cb.config.CircuitBreakerAutoConfiguration; import
+ * com.tsb.cb.config.CircuitBreakerConfigProperties; import
+ * com.tsb.cb.config.CircuitBreakerProperties;
+ * 
+ * @RunWith(SpringJUnit4ClassRunner.class)
+ * 
+ * @ContextConfiguration(classes = {CircuitBreakerApplication.class,
+ * CircuitBreakerAutoConfiguration.class })
+ * 
+ * @WebAppConfiguration //@EnableAutoConfiguration()
+ * 
+ * @EnableConfigurationProperties(value = { CircuitBreakerProperties.class})
+ * 
+ * @TestPropertySource(locations="classpath:application-test.yml")
+ * 
+ * public class AppTests {
+ * 
+ * 
+ * public static void main(String[] args) {
+ * 
+ * }
+ * 
+ * }
+ */
